fcf306e30420af217a13db7d32a15fa396f05596
kushansharma <kushansharma654321@gmail.com>
2020-01-29 12:20:09 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const io = require('socket.io')(2000)
+
+const users = {}
+
+ io.on('connection', socket =>{
+     socket.on('new user', name => {
+         users[socket.id] = name
+         socket.broadcast.emit('user-connected', name)
+     })
+
+    
+     socket.on('send-chat-message', message => {
+         socket.broadcast.emit('chat-message', {message: message, name:users[socket.id]
+        })
+     })
+
+     socket.on('user-disconnect', name => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+ })
